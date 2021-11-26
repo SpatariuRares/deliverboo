e3e32adb96df6189c7ca9b582fe216c43b2a70bf
spata00 <sararares12@gmail.com>
2021-11-26 10:56:27 +0100

    crud per il guest

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\Category;
 use Illuminate\Http\Request;
 
@@ -14,72 +15,16 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $Categories = Category::all();
+        return view('guest.Categories.index', compact('Categories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
     public function show(Category $category)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Category $category)
-    {
-        //
+        if(!$category) {
+            abort(404);
+        }
+        $user = User::find($category);
+        return view("user.categories.show", compact("category"));
     }
 }
--- database/seeds/FoodSeeder.php
+++ database/seeds/FoodSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Faker\Generator as Faker;
 use App\Food;
 use App\User;
+
 class FoodSeeder extends Seeder
 {
     /**
