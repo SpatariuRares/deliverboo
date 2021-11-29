a1b6e010ae0f7a6ec03ef6e11af86b68e9da101b
Lorenzo <baccini.lorenzo@gmail.com>
2021-11-29 17:17:38 +0100

    Delete account aggiunto

--- resources/views/guest/restaurant/index.blade.php
+++ resources/views/guest/restaurant/index.blade.php
@@ -16,27 +16,29 @@
     <div class="container-fluid">
         <div class="row g-3 d-flex">
                 @foreach ($users as $user)
-                    <div class="col-3">
-                        <div class="img bg-dark rounded">
-                            <img src="" alt="">
+                    @if($user["slug"] != null)
+                        <div class="col-3">
+                            <div class="img bg-dark rounded">
+                                <img src="" alt="">
+                            </div>
+                            <div class="description py-2">
+                                @dump($user['slug'])
+                                <a href="{{ route('show', $user['slug']) }}">
+                                    <h4>{{ $user['username'] }}</h4>
+                                </a>
+                                <p class="lh-1">
+                                    @foreach ($user->categories as $category)
+                                        @if ($category == $user->categories->last())
+                                            <a class="text-reset" href="{{ route('categories.show', $category['id']) }}">{{ $category->name }}</a>
+                                        @else
+                                            <a class="text-reset" href="{{ route('categories.show', $category['id']) }}">{{$category->name.', '}}</a>
+                                        @endif
+                                    @endforeach
+                                </p>
+                                <p class="lh-1">{{ $user['address'] }}</p>
+                            </div>
                         </div>
-                        <div class="description py-2">
-                            @dump($user['slug'])
-                            <a href="{{ route('show', $user['slug']) }}">
-                                <h4>{{ $user['username'] }}</h4>
-                            </a>
-                            <p class="lh-1">
-                                @foreach ($user->categories as $category)
-                                    @if ($category == $user->categories->last())
-                                        <a class="text-reset" href="{{ route('categories.show', $category['id']) }}">{{ $category->name }}</a>
-                                    @else
-                                        <a class="text-reset" href="{{ route('categories.show', $category['id']) }}">{{$category->name.', '}}</a>
-                                    @endif
-                                @endforeach
-                            </p>
-                            <p class="lh-1">{{ $user['address'] }}</p>
-                        </div>
-                    </div>  
+                    @endif  
                 @endforeach
             </div>
         </div>
