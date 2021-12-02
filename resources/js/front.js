d168e427152b1c8dc1cf3f33aeff9f435cc63df4
spata00 <sararares12@gmail.com>
2021-12-02 22:54:50 +0100

    id menu ripristinato

--- resources/js/front.js
+++ resources/js/front.js
@@ -4,7 +4,7 @@ window.axios = require('axios');
 import vueBraintree from 'vue-braintree'
 import BootstrapVue from 'bootstrap-vue' //Importing
 
-
+import Restaurant from './views/Restaurant';
 import Menu from './views/Menu';
 Vue.use(vueBraintree)
 Vue.use(BootstrapVue)
@@ -14,19 +14,19 @@ Vue.use(BootstrapVue)
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const Menu= new Vue({
+const menu= new Vue({
     el: '#menu',
     render: h =>h(Menu),
 });
 
-import Restaurant from './views/Restaurant';
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const Restaurant = new Vue({
+const restaurant = new Vue({
     el: '#restaurant',
     render: h =>h(Restaurant)
 });
