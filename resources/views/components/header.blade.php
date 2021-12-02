716370aaa463ff3d64e196d20abce872f905c69b
spata00 <sararares12@gmail.com>
2021-12-02 14:48:54 +0100

    miglioramento header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,44 +1,46 @@
 <header>
-    <div class="container-fluid">
-        <div class="row p-2">
-            <div class="col-3 d-flex justify-content-center">
+    <div class="container">
+        <div class="row p-2 justify-content-around">
+            <div class="d-inline-flex justify-content-center">
                 <a href="{{ route('index') }}"> logo</a>
             </div>
-            <div class="col">
+            <div class="col-6">
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
-            @guest
-                <div class="col-3 d-flex justify-content-center">
-                    <a class="btn btn-success mx-3" href="{{ route('login') }}">Accedi</a>
-                    @if (Route::has('register'))
-                        <a class="btn btn-success mx-3" href="{{ route('register') }}">Registrati</a>
-                    @endif
-                </div>
-                @else
-                <div class="col-3 d-flex justify-content-center">
-                    <ul class="list-group list-unstyled flex-row">
-
-                        <li class="nav-item ">
-                            <a class="nav-link" href="{{ route('user.restaurant.index')}}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="btn btn-danger" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    Logout
-                            </a>
-                        </li>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form> 
-                    </ul>
-                </div>           
-            @endguest 
+            <div>
+                @guest
+                    <div class=" d-inline-flex justify-content-center">
+                        <a class="btn btn-success mx-3" href="{{ route('login') }}">Accedi</a>
+                        @if (Route::has('register'))
+                            <a class="btn btn-success mx-3" href="{{ route('register') }}">Registrati</a>
+                        @endif
+                    </div>
+                    @else
+                    <div class="d-inline-flex justify-content-center">
+                        <ul class="list-group list-unstyled flex-row">
+    
+                            <li class="nav-item ">
+                                <a class="nav-link" href="{{ route('user.restaurant.index')}}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="btn btn-danger" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        Logout
+                                </a>
+                            </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form> 
+                        </ul>
+                    </div>           
+                @endguest 
+            </div>
         </div>
     </div>
 </header>
