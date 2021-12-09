a730d4f1da51eeb36a0aa517a56ffedc6c875ca5
spata00 <sararares12@gmail.com>
2021-12-09 12:19:13 +0100

    visibli

--- resources/js/views/Restaurant.vue
+++ resources/js/views/Restaurant.vue
@@ -61,23 +61,25 @@ export default {
       });
       if(cartFlag){
         axios.post("http://127.0.0.1:8000/api/food/cart",{ ...this.form }).then((response) => {
-          if(this.cart.length > 0) {
-            if(response.data.cart["user_id"] == this.cart[0]["user_id"]){
-                this.cart.push(response.data.cart);
-                this.reset=false;
+          if(response.data.cart.visible!=0){
+            if(this.cart.length > 0) {
+              if(response.data.cart["user_id"] == this.cart[0]["user_id"]){
+                  this.cart.push(response.data.cart);
+                  this.reset=false;
+                }
+                else{
+                  // console.log("2",this.cart[0]["id"])
+                  this.cart=[];
+                  this.reset=true;
+                  this.cart.push(response.data.cart);
+                }
               }
               else{
-                // console.log("2",this.cart[0]["id"])
-                this.cart=[];
                 this.reset=true;
                 this.cart.push(response.data.cart);
               }
-            }
-            else{
-              this.reset=true;
-              this.cart.push(response.data.cart);
-            }
-          this.savecart();
+            this.savecart();
+          }
         })
       }
     },
