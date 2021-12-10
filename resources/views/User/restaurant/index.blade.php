88cf80908e089934b8865cf59ad53bcdfca928c6
SimoneBenato <sb270298@gmail.com>
2021-12-10 11:05:35 +0100

    Integrazioni grafiche utente registrato

--- resources/views/User/restaurant/index.blade.php
+++ resources/views/User/restaurant/index.blade.php
@@ -10,31 +10,22 @@
         <div class="col-12 d-flex justify-content-between">
             <div class="">
                 <button class="btn p-2 btn-success">
-                    <a class="text-white" href="{{ route('user.orders.index') }}">
+                    <a class="text-white text-decoration-none" href="{{ route('user.orders.index') }}">
                         Visualizza i tuoi ordini
                     </a>
                 </button>
             </div>
             <div class="">
                 <button class="btn p-2 btn-success">
-                    <a class="text-white" href="{{ route('user.foods.index') }}">
+                    <a class="text-white text-decoration-none" href="{{ route('user.foods.index') }}">
                         Visualizza i tuoi cibi
                     </a>
                 </button>
             </div>
             <div class="">
-                <button class="btn p-2 btn-success">
-                    <a class="text-white" href="{{ route('user.foods.create') }}">
-                        Aggiungi un piatto al menù
-                    </a>
-                </button>
-            </div>
-            <div class="">
-                <button class="btn p-2 btn-outline-secondary">
-                    <a class="text-warning" href="{{ route('user.restaurant.edit', $user['slug']) }}">
-                        Modify
-                    </a>
-                </button>
+                <a class="text-decoration-none btn p-2 btn-outline-info" href="{{ route('user.restaurant.edit', $user['slug']) }}">
+                    Modify
+                </a>
             </div>
             <form class="" method="post" onclick="return confirm('Questa azione è irreversibile!!! Sei sicuro di voler cancellare?')" action="{{ route('user.restaurant.destroy', $user['id']) }}">
                 @csrf
